1f2440c0adf5229fcd5686fceb81fd52aceac3a6
muthuselvi67 <muthuselvi.s2910@gmail.com>
2025-06-16 12:01:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "crud_db";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
 
